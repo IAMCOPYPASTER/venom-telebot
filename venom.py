9a4da9e4a2123bd794e75623c284921182753df1
IAMCOPYPASTER <79563575+IAMCOPYPASTER@users.noreply.github.com>
2024-10-07 13:26:22 +0530

    venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '7858781054:AAHkCrGRBMN8AN3IL7LcQff_fc7woe_mn7w'
+ADMIN_ID = 'Rezxtion'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
